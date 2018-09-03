9da9c43fe84d0bd03861bcec48d51d339dd168a0
realityone <realityone@me.com>
2018-09-03 12:01:55 +0800

    integrate with nk4

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,9 @@
 extern crate libc;
 extern crate netkeeper;
 
-use std::ffi::CStr;
-use netkeeper::netkeeper::dialer::{NetkeeperDialer, Configuration};
 use netkeeper::common::dialer::Dialer;
+use netkeeper::netkeeper4::dialer::{Configuration, Netkeeper4Dialer};
+use std::ffi::CStr;
 
 extern "C" {
     static mut user: [libc::c_schar; 256];
@@ -15,16 +15,22 @@ pub extern "C" fn plugin_init() {
 
     let origin;
     unsafe {
-        origin = CStr::from_ptr(&user as *const i8).to_string_lossy().into_owned();
+        origin = CStr::from_ptr(&user as *const i8)
+            .to_string_lossy()
+            .into_owned();
     }
     println!("Origin username is: {:?}", origin);
 
-    let dialer = NetkeeperDialer::load_from_config(Configuration::Zhejiang);
+    let dialer = Netkeeper4Dialer::load_from_config(Configuration::Zhejiang);
     let encrypted = dialer.encrypt_account(&origin, None);
 
     println!("Encrypted username is: {:?}", encrypted);
     unsafe {
-        let encrypted_bytes = encrypted.as_bytes().iter().map(|c| *c as i8).collect::<Vec<i8>>();
+        let encrypted_bytes = encrypted
+            .as_bytes()
+            .iter()
+            .map(|c| *c as i8)
+            .collect::<Vec<i8>>();
         user[0..encrypted.len()].copy_from_slice(&encrypted_bytes);
     }
 }
